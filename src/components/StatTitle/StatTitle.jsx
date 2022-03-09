7d0bf9131322cb86ba2cdfb3dd264fe4f0a1ab14
Bohdan-Vozniak <bogdan2907@gmail.com>
2022-03-09 20:21:28 +0200

    add

--- src/components/StatTitle/StatTitle.jsx
+++ src/components/StatTitle/StatTitle.jsx
@@ -2,9 +2,9 @@ import PropTypes from 'prop-types';
 import css from './StatTitle.module.css';
 
 export const StatTitle = ({ title }) => {
-  return <h2 className={css.title}>{title}</h2>;
+  return title ? <h2 className={css.title}>{title}</h2> : '';
 };
 
 StatTitle.propTypes = {
-  title: PropTypes.string.isRequired,
+  title: PropTypes.string,
 };
